f0d93569a7f069fe4c13b5064ec55b4fb7bf9f06
Marcos Santana <86273127+marcos-santan4@users.noreply.github.com>
2022-10-28 00:11:32 -0300

    Update script.js

--- Code/assets/js/script.js
+++ Code/assets/js/script.js
@@ -86,24 +86,3 @@ function save() {
     var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
     saveAs(blob, name);
 }
-// const setB = new Set();
-
-// setA.add('Banana');
-// setA.add('Uva');
-// setA.add('Laranja');
-// setA.add('Maça');
-// setA.add('Pera');
-
-// setA.delete('Pera'); // pq pera é rui
-
-// console.log(setA);
-
-// setB.add('Alface');
-// setB.add('Cebolinha');
-// setB.add('Couve');
-// setB.add('Salsa');
-// setB.add('Coentro');
-
-// console.log(setB);
-
-// console.log(setA.union(setB).values());
